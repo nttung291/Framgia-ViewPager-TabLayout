0ba55d88be34cc606416c2de808f58818eae9940
nttung291 <nttung291@gmail.com>
2018-01-25 10:46:51 +0700

    ViewPager-TabLayout

--- app/src/main/java/com/framgia/framgia_viewpager_tablayout/MainActivity.java
+++ app/src/main/java/com/framgia/framgia_viewpager_tablayout/MainActivity.java
@@ -6,6 +6,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+
     private TabLayout tabLayout;
     private ViewPager viewPager;
 
